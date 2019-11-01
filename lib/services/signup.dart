fd71b019d97ad2d4178c5a88fdc49aba18fe69f7
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2019-11-01 03:14:43 +0200

    Updated code

--- lib/services/signup.dart
+++ lib/services/signup.dart
@@ -1,6 +1,7 @@
 import '../models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 dynamic createUser(BuildContext context, CreateUserModel createUserModel) async {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
@@ -14,11 +15,18 @@ dynamic createUser(BuildContext context, CreateUserModel createUserModel) async 
     Navigator.pop(context);
     print(result.user.uid);
 
+    Firestore.instance.collection("user").document().setData({
+      "uid": result.user.uid,
+      "username": createUserModel.username,
+      "email": createUserModel.email,
+      "designation": createUserModel.designation
+    });
+
     if(!result.user.isEmailVerified){
       result.user.sendEmailVerification();
     }
 
-    
+
 
     return null;
 
